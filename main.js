8bbcd142485e28e5d9a33113ceafa09b26a39182
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-03-21 10:19:16 -0600

    add script and anchors to mobile links

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ document.querySelectorAll(".js-mobile-menu").forEach((btn) => {
   });
 });
 
-// modal window
+//close mobile menu by clicking on a link
+document.querySelectorAll(".mb-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.querySelector(".mb-menu").classList.toggle("is-open");
+  });
+});
+
+// to open and close modal window
 document.querySelectorAll(".js-modal-window").forEach((btn) => {
   btn.addEventListener("click", () => {
     document.querySelector(".modal-overlay").classList.toggle("is-open");
